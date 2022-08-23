69235605c53e5f97ebfb35dbe215d8d8cfffef27
FabioLongoni <longoni.fabio90@gmail.com>
2022-08-23 15:03:53 +0200

    bonus

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = new Vue({
     el: '#root',
     data: {
         title: 'surf-hawai',
+        img: 'https://dbdzm869oupei.cloudfront.net/img/sticker/preview/28723.png',
     },
     methods: {
         toUpper(text){
